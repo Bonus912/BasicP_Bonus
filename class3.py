8c452e3deb6a24fd9d1f5a626c35862ea4446b85
Bonus912 <bonussi2549@gmail.com>
2025-07-23 11:16:57 +0700

    add class3

---
+++ class3.py
@@ -0,0 +1,83 @@
+# haverice = True
+# havespoon = False
+# havehand = True
+
+# if haverice:
+#     if havespoon:
+#         print("กินข้าว")
+#     elif havehand:
+#         print("กินข้าวเหนียว")
+
+# score = int(input("Enter your score"))
+    
+# if 100 > score >= 80:
+#     print(" A ")
+# if 80 > score >= 70:
+#     print(" B ")
+# if 70 > score >= 60:
+#     print(" C ")
+# if 60 > score >= 50:
+#     print(" D")
+
+# score = int(input("Enter your score"))
+
+# if score >= 0:
+#     if score <= 100:
+#         if score >= 80:
+#             print(" A ")
+#         if score >= 70:
+#             if score < 80:
+#                 print(" B ")
+#         if score >= 60:
+#             if score < 70:
+#                 print(" C ")
+#         if score >= 50:
+#             if score < 60:
+#                 print(" D ")
+#         if score < 50:
+#             print(" F ")
+
+# for i in range(5):
+#     print("สวัสดี")
+
+# for i in range(8):
+#     print("สวัสดี")
+
+# for i in range(1,5):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+# i = 0
+# while i < 5:
+#     print("สวัสดี")
+#     i = i + 1
+
+# i = 0
+# while i < 8:
+#     print("สวัสดี")
+#     i = i + 1
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     i = i + 1
+
+
+while True:
+    choice = int(input("กรอก 1 เพื่อบวกเลข, กรอก 2 เพื่อออก"))
+
+    if choice == 1:
+        num = int(input("จำนวนเลขที่ต้องการจะบวก:"))
+        sumation = 0
+
+        for i in range(num):
+            num1 = int(input("กรอกเลข:"))
+            sumation = sumation + num1
+        
+        print("ผลลัพท์", sumation)
+
+    if choice == 2:
+        print("บาย บาย")
+        break
