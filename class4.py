55217f0acae8e9ea5cae4c699855aee53b5fdc5e
Bonus912 <bonussi2549@gmail.com>
2025-07-24 12:02:16 +0700

    add class4

---
+++ class4.py
@@ -0,0 +1,93 @@
+# def add():
+#     print(5 + 5)
+
+# add()
+
+# def grade(score):
+#     if score >= 80:
+#         print(" Grade A ")
+#     elif score >= 70:
+#         print(" Grade B ")
+#     elif score >= 60:
+#         print(" Grade C ")
+
+# grade(60)
+
+# def my_sum(a,b):
+#     print( a+b )
+
+# my_sum(5,5)
+
+# def my_sum( a , b ):
+#      return a + b 
+
+# result = my_sum(5 ,5)
+# print(result)
+
+# def sumation(quan , per):
+#     result = quan * per
+#     total_vat = result * 0.07
+#     total = result + total_vat
+#     return total
+
+
+# def choosemenu():
+#     while True:
+#         choice = int(input(" 1. เลือกเพื่อคำนวนราคาสินค้า 2. เลือกเพื่อออก"))
+#         if choice == 1:
+#             quan = int(input(" จำนวนสินค้า "))
+#             per = int(input(" ราคาต่อชิ้น "))
+#             print(sumation(quan , per))
+
+#         if choice == 2:
+#             print("บาย")
+#             break
+
+# choosemenu()
+
+
+# my_list = [1, 2, "Bo"]
+
+# result = my_list[0]
+# print(result)
+
+# my_list[0] = 3
+# print(my_list[0])
+
+
+# my_list = [1, 2, "Bo"]
+
+# result = my_list[0]
+# print(my_list)
+
+# my_list[0] = 3
+# print(my_list)
+
+# print(my_list)
+
+# # my_list.append("BB")
+
+# my_list.pop(0)
+
+# print(my_list)
+
+# for i in my_list:
+#     print(i)
+
+# my_dict = {"name" : "Bonus", "age" : 18}
+# print(my_dict)
+# print(my_dict["name"])
+
+# my_dict["It"] = 30
+# print(my_dict)
+
+# my_dict["name"] = "Bo"
+# print(my_dict)
+
+# students = [
+#     {"name" : "Bonus" , "age" : 18},
+#     {"name" : "Lek" , "age" : 19}
+# ]
+
+# for i in students:
+#     print(i["age"])
